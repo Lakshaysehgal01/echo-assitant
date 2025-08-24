b86d0303afc92b4e9dd5b2a27ee4e9e369c03cf0
Lakshaysehagl01 <sehgall333@gmail.com>
2025-08-24 23:26:01 +0530

    Widget header & footer

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,26 +1,10 @@
 "use client";
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
-import { Button } from "@workspace/ui/components/button";
-export default function Page() {
-  const {
-    isConnected,
-    isConnecting,
-    isSpeaking,
-    transcript,
-    startCall,
-    endCall,
-  } = useVapi();
-  console.log(isConnected);
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
-      <Button onClick={() => startCall()}>Start Call</Button>
-      <Button onClick={() => endCall()} variant={"destructive"}>
-        End Call
-      </Button>
-      <p>Is Connected :{isConnected ? "true" : "false"}</p>
-      <p>Is Connecting :{isConnecting ? "true" : "false"}</p>
-      <p>Is Speaking :{isSpeaking ? "true" : "false"}</p>
-      {JSON.stringify(transcript, null, 2)}
-    </div>
-  );
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
+import { use } from "react";
+interface Props {
+  searchParams: Promise<{ organizationId: string }>;
+}
+export default function Page({ searchParams }: Props) {
+  const { organizationId } = use(searchParams);
+  return <WidgetView organizationId={organizationId} />;
 }
