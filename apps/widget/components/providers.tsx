89d9a1426ce5f702a0d3d779fe27bde2a642ae60
Lakshaysehagl01 <sehgall333@gmail.com>
2025-08-27 18:10:39 +0530

    Jotai screen routing added

--- apps/widget/components/providers.tsx
+++ apps/widget/components/providers.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Provider } from "jotai";
 import * as React from "react";
 const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={client}>{children}</ConvexProvider>;
+  return (
+    <ConvexProvider client={client}>
+      <Provider>{children}</Provider>
+    </ConvexProvider>
+  );
 }
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,16 +1,30 @@
 "use client";
 
+import { useAtomValue } from "jotai";
 // import { WidgetFooter } from "../components/widget-footer";
 // import { WidgetHeader } from "../components/widget-header";
-import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import { screenAtom } from "@/modules/widget/atoms/widget-atom";
 
 interface Props {
   organizationId: string;
 }
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>Error Screen</p>,
+    loading: <p>Loading Screen</p>,
+    selection: <p>Selection Screen</p>,
+    voice: <p>Voice Screen</p>,
+    auth: <WidgetAuthScreen />,
+    inbox: <p>Inbox Screen</p>,
+    chat: <p>Chat Screen</p>,
+    contact: <p>Contact Screen</p>,
+  };
   return (
     <main className="min-h-screen  flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
+      {screenComponents[screen]}
       {/* <WidgetFooter /> */}
     </main>
   );
